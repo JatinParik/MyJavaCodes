da47371498c5f9a84a9245398009594d920eb89f
JatinParik <jatinpareek5324@gmail.com>
2025-07-03 19:20:39 +0530

    Solid Rectangle Pattern

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        for( int i = 0 ; i<4 ; i++) {
+            for(int j = 0 ; j<5 ; j++) {
+            System.out.print("*");
+          } System.out.print("\n");
+        }   
+      }   
+}
